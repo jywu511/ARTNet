6c338016af41c8312a731bda88d0cfd05c3a0de7
Junyang Wu <91044076+jywu511@users.noreply.github.com>
2025-09-03 14:36:56 +0800

    Update ART_model.py

--- models/ART_model.py
+++ models/ART_model.py
@@ -8,15 +8,8 @@ import itertools
 import pickle 
 from util.image_pool import ImagePool
 
-class CUTModel(BaseModel):
-    """ This class implements CUT and FastCUT model, described in the paper
-    Contrastive Learning for Unpaired Image-to-Image Translation
-    Taesung Park, Alexei A. Efros, Richard Zhang, Jun-Yan Zhu
-    ECCV, 2020
+class ARTModel(BaseModel):
 
-    The code borrows heavily from the PyTorch implementation of CycleGAN
-    https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
-    """
     @staticmethod
     def modify_commandline_options(parser, is_train=True):
         """  Configures options specific for CUT model
